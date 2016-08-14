8a0426553e53dc52b005ab457e89f6164e7e3135
Paul Herold Jr <pjhd12@mit.edu>
2016-08-14 12:35:25 -0400

    first commit

---
+++ server/server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var path = require("path");
+//var animalController = require("./controllers/animalController");
+
+//Express request pipeline
+var app = express();
+app.use(express.static(path.join(__dirname, "../app/dist")));
+app.use(bodyParser.json())
+//app.use("/api", animalController);
+
+var server_port = process.env.PORT || 3000;
+app.listen(server_port, function() {
+	console.log("Started listening on port", server_port);
+});
+
+// Connect to mongodb database
+mongoose.connect("mongodb://localhost/tvshowtracker");
