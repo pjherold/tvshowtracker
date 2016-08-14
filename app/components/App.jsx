35c7f9f9782c5eb74908b08d0acd55364104d01c
Paul Herold Jr <pjhd12@mit.edu>
2016-08-14 19:12:38 -0400

    navigation bar set up

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -7,19 +7,16 @@ class App extends React.Component{
     		<Navbar>
     			<Navbar.Header>
       				<Navbar.Brand>
-        				<a href="#">React-Bootstrap</a>
+        				<a>TVShowTracker</a>
       				</Navbar.Brand>
     			</Navbar.Header>
     			<Nav>
-      				<NavItem eventKey={1} href="#">Link</NavItem>
-				     <NavItem eventKey={2} href="#">Link</NavItem>
-				     <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-				        <MenuItem eventKey={3.1}>Action</MenuItem>
-				        <MenuItem eventKey={3.2}>Another action</MenuItem>
-				        <MenuItem eventKey={3.3}>Something else here</MenuItem>
-				        <MenuItem divider />
-				        <MenuItem eventKey={3.3}>Separated link</MenuItem>
-				     </NavDropdown>
+      				<NavItem eventKey={1} href="#">Home</NavItem>
+				    <NavItem eventKey={2} href="#">Add</NavItem>
+				</Nav>
+				<Nav pullRight>
+				    <NavItem eventKey={3} href="#">Login</NavItem>
+				    <NavItem eventKey={4} href="#">Sign Up</NavItem>
 			    </Nav>
 			 </Navbar>
     	);
